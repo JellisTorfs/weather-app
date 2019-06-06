6ed2e2915267a484f6c8c44aa5693d72667e4e51
Torfs Jellis <torfsje@cronos.be>
2019-06-06 14:13:19 +0200

    Removed useless console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('Client sided file loaded.');
-
-
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#firstP');
